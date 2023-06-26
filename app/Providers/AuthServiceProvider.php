7c265306fbce99709515a868a404367c39f8e757
as247 <as247@vui360.com>
2023-06-26 11:10:52 +0700

    Update skeleton for 1.6.1

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,9 +2,26 @@
 
 namespace App\Providers;
 
-use WpStarter\Support\ServiceProvider;
-
+use WpStarter\Auth\AuthManager;
+use WpStarter\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 class AuthServiceProvider extends ServiceProvider
 {
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
 
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot(AuthManager $authManager)
+    {
+        $this->registerPolicies();
+    }
 }
--- config/app.php
+++ config/app.php
@@ -202,9 +202,9 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        //App\Providers\AuthServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
-        //App\Providers\EventServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Admin\AdminServiceProvider::class,
 
